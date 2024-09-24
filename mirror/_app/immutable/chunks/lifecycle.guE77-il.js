4141cf1ce183b6c367abaf1283fdfbb6db83b42e
Felix Sewing <121240566+fsewing@users.noreply.github.com>
2024-09-24 20:03:00 +0200

    add mirror build static

---
+++ mirror/_app/immutable/chunks/lifecycle.guE77-il.js
@@ -0,0 +1,1 @@
+import{u as r,e,h as c,i,j as l,k as a,g as u,l as _}from"./runtime.ClRJlCNk.js";function g(){const s=c,n=s.l.u;n&&(n.b.length&&r(()=>{f(s),a(n.b)}),e(()=>{const o=i(()=>n.m.map(l));return()=>{for(const t of o)typeof t=="function"&&t()}}),n.a.length&&e(()=>{f(s),a(n.a)}))}function f(s){if(s.l.s)for(const n of s.l.s)u(n);_(s.s)}export{g as i};
